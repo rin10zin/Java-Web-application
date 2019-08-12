0262435060e038e90a0f658832445671a12464c8
SHERPA RINZIN <45703022+rin10zin@users.noreply.github.com>
2019-08-12 11:24:52 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+for (int A = 12; A > 9 ; A--) {
+   // int A = 12;
+    double inchVal = A;
+    double meterVal = A * 0.1;
+//while(A < 15){
+
+    System.out.println(inchVal + "inches  = " + meterVal + "met");
+   // A ++;
+    }
+    }
+}
